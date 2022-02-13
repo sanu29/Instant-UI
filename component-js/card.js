97666122f7229e7932acde1988966b7234ad5e20
Saniya Anush Shaikh <sanu29121999@gmail.com>
2022-02-13 14:27:43 +0530

    Added : Documentation for buttons and cards

---
+++ component-js/card.js
@@ -0,0 +1,5 @@
+document.querySelector("#dismiss-card-btn").addEventListener("click",()=>{
+    const cardDismis = document.getElementById("dismiss-card")
+    cardDismis.style.display="none";
+ })
+ 
